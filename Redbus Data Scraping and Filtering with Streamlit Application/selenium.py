6b6b30b4ed30c243d14e5d40be0b9eb5f375c9d0
Bavankumar2002 <bavankumar2002@gmail.com>
2024-07-23 22:33:38 +0530

    Update selenium.py

--- Redbus Data Scraping and Filtering with Streamlit Application/selenium.py
+++ Redbus Data Scraping and Filtering with Streamlit Application/selenium.py
@@ -1,8 +1,14 @@
 from selenium import webdriver
+from selenium.webdriver import chrome
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import time
 import pandas as pd
+import os
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
 import sqlite3
 
 
